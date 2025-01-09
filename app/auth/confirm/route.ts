545c3394317edc2e64d156c54736fde3a151b292
root <root@AR.localdomain>
2025-01-09 15:11:20 +0300

    Added returns for Response Objects

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,10 +1,9 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest } from 'next/server'
-
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest): Promise<Response> {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
@@ -18,11 +17,11 @@ export async function GET(request: NextRequest) {
       token_hash,
     })
     if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next)
+      // Return a response that redirects to the next URL
+      return Response.redirect(new URL(next, request.url))
     }
   }
 
-  // redirect the user to an error page with some instructions
-  redirect('/error')
+  // Return a response that redirects to the error page
+  return Response.redirect(new URL('/error', request.url))
 }
